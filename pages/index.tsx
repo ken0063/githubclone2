e9531ec76b335fa2b4419187aadc93c232225b8b
ken0063 <olukotunk@gmail.com>
2022-02-16 11:11:45 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = (): JSX.Element => {
+  return <div>
+    Hello World!!!
+  </div>;
+};
+
+export default Home;
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+
+const Login: NextPage = (): JSX.Element => {
+ 
+  return <input />;
+};
+
+export default Login;
